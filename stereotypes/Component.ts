df83450bb7db895c7a2cdbd95a6b906a0770f7b5
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2022-02-19 19:37:12 -0300

    test: remove stereotypes

--- stereotypes/Component.ts
+++
@@ -1,8 +0,0 @@
-import { Token } from '../Token.js'
-import { Injectable } from '../decorators/Injectable.js'
-
-export function Component<T>(token?: Token<T>) {
-  return function <TFunction extends Function>(target: TFunction | object, propertyKey?: string | symbol) {
-    return Injectable(token)(target, propertyKey)
-  }
-}
--- stereotypes/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Component.js'
-export * from './Repository.js'
-export * from './Service.js'
-export * from './UseCase.js'
