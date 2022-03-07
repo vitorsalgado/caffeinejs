54c268b2c15c27b9f9b083c90b983d46cbf0e8a0
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2022-03-07 02:05:10 -0300

    chore: upgrade deps

--- web/WebAdapter.ts
+++ web/WebAdapter.ts
@@ -3,22 +3,23 @@ import { ApplicationContext } from '@caffeinejs/application'
 import { Environment } from '@caffeinejs/application/dist/env/Environment.js'
 import { R } from '@caffeinejs/std'
 import Fastify from 'fastify'
+import { InitContext } from '@caffeinejs/application'
 import { Vars } from './internal/Vars.js'
 
 const fastify = Fastify()
 
-export abstract class WebAdapter implements Adapter {
-  bootstrap(context: ApplicationContext): Promise<void> {
+export abstract class WebAdapter implements Adapter<ApplicationContext> {
+  setupEnvironment(environment: Environment): Promise<Environment> {
+    return Promise.resolve(environment)
+  }
+
+  bootstrap(context: InitContext): Promise<void> {
     const controllers = context.container.search(token => R.hasOwn(Vars.CONTROLLER, token))
 
     return Promise.resolve(undefined)
   }
 
-  setupApplicationContext(context: ApplicationContext): ApplicationContext {
-    return context
-  }
-
-  setupEnvironment(environment: Environment): Environment {
-    return environment
+  adapt(context: ApplicationContext): Promise<ApplicationContext> {
+    throw new Error('Method not implemented.')
   }
 }
